cfcd0cde4fc361a80cf5dd9da576c3e8628c6a6a
Hyeon Kim <simnalamburt@gmail.com>
2016-01-03 18:56:20 +0900

    Implement fasd_cd

--- fasd.fish
+++ fasd.fish
@@ -1,13 +1,35 @@
 function init --on-event init_fasd
+  # Detect fasd
   if not available fasd
     echo "🍒  Please install 'fasd' first!"
-  else
-    function -e fish_preexec _run_fasd
-      fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
-    end
+    return
+  end
 
-    function j
-      cd (fasd -d -e 'printf %s' "$argv")
+  #
+  # Fish hooks
+  #
+  function -e fish_preexec _run_fasd
+    fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
+  end
+
+  function fasd_cd -d 'Function to execute built-in cd'
+    # if no $argv, identical with `fasd`
+    set -l N (count $argv ^ /dev/null)
+    if [ "$N" -le 1 ]
+      fasd "$argv"
+    else
+      set -l ret (fasd -e 'printf %s' $argv)
+      [ -z "$ret" ]; and return
+      [ -d "$ret" ]; and cd "$ret"; or printf "%s\n" $ret
     end
   end
+
+  alias a='fasd -a'
+  alias s='fasd -si'
+  alias sd='fasd -sid'
+  alias sf='fasd -sif'
+  alias d='fasd -d'
+  alias f='fasd -f'
+  alias z='fasd_cd -d'
+  alias zz='fasd_cd -di'
 end
