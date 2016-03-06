802082fa1ba53decaef35abf360c6efccf6599ee
Justin Hileman <justin@justinhileman.info>
2016-03-06 12:45:48 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,12 +1,9 @@
-function init --on-event init_fasd
-  # Detect fasd
-  if not type -q fasd
-    echo "🍒  Please install 'fasd' first!"
-    return
-  end
-
+# Detect fasd
+if type -q fasd
   # Hook into fish preexec event
   function __fasd_run -e fish_preexec
     command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1 &
   end
+else
+  echo "🍒  Please install 'fasd' first!"
 end
