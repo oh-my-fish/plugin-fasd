64b4af5172e6ea575e66d8892e06f6a5bea79885
Edward Misback <elmisback@users.noreply.github.com>
2016-09-17 19:43:52 -0300

    Separate args to `fasd --proc`.
    See [here](https://github.com/fish-shell/fish-shell/issues/1358#issuecomment-76447368) for details.

--- init.fish
+++ init.fish
@@ -2,7 +2,7 @@
 if type -q fasd
   # Hook into fish preexec event
   function __fasd_run -e fish_preexec
-    command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1 &
+    command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &
   end
 else
   echo "🍒  Please install 'fasd' first!"
